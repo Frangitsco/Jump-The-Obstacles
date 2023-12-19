7d9a7a62b07d18e9c6668a0571a43d9b0fabbad2
Francesco Ponzo <mr.franceso.ponzo@gmail.com>
2023-12-19 00:37:40 +0000

    implement sound and music

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private Animator playerAnim;
+    private AudioSource playerAudio;
     public float jumpforce;
     public float gravityModifier;
     public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip crashSound;
+    public AudioClip jumpSound;
     public bool isOnGround = true;
     public bool gameOver = false;
     // Start is called before the first frame update
@@ -17,6 +21,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier; 
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,18 +31,23 @@ public class PlayerController : MonoBehaviour
          playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
          isOnGround = false; 
          playerAnim.SetTrigger("Jump_trig");
+         dirtParticle.Stop();
+         playerAudio.PlayOneShot(jumpSound, 2.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision){
         if (collision.gameObject.CompareTag("Ground")){
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle")){
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
         }
     }
